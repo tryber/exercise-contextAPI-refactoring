dfbb1b4fa33b66f0ba130209d1c7ee36290f0815
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-04-13 18:33:07 -0400

    remove redux and use context api instead

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css';
+import MyContext from './MyContext';
 import Cars from './Cars';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    };
+    this.handleCars = this.handleCars.bind(this);
+  }
+
+  handleCars(carColor, newStateCar) {
+    this.setState((prevState) => (
+      { cars: { ...prevState.cars, [carColor]: newStateCar }})
+    );
+  }
+
+  render() {
+    const contextValue = { 
+      cars: this.state,
+      moveCar: this.handleCars,
+    };
+    return (
+      <MyContext.Provider value={contextValue}>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
